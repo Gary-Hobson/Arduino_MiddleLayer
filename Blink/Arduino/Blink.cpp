d5b402799318759c11a4af0e3cf1931191d718d9
garyhobson <garyhobson@qq.com>
2019-09-28 15:02:53 +0800

    提交

---
+++ Blink/Arduino/Blink.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PB14, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(PB14, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(PB14, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
+}
